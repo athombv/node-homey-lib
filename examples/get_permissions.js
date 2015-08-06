50484d90b3319116539fd6c625348c07fce8aa1b
Emile Nijssen <emilenijssen@gmail.com>
2015-08-06 13:33:22 +0200

    added permissions check

---
+++ examples/get_permissions.js
@@ -0,0 +1,4 @@
+var devkit_lib = require('..');
+
+var permissions = devkit_lib.permissions.getPermissions();
+console.log( permissions );
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports.App = require('./lib/app.js');
+var path = require('path');
+
+module.exports.App = require('./lib/app/index.js');
+
+module.exports.permissions = require('./lib/permissions/index.js');
