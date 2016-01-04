0a3e380f60dc14abfc469809f09e14b2ba9a16c7
Emile Nijssen <emilenijssen@gmail.com>
2016-01-04 17:16:24 +0100

    fixes

--- examples/validate_app.js
+++ examples/validate_app.js
@@ -5,7 +5,7 @@ if( typeof process.argv[2] == 'undefined' ) {
 }
 
 var app = new lib.App( process.argv[2] );
-var result = app.validate( true ); // true = check for app store
+var result = app.validate( ( process.argv[3] == '--validate-app-store' ) || false ); // true = check for app store
 
 if( result.success ) {
 	console.log('valid app!');
