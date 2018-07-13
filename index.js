2e329fa8be78fe7d1e4a50890ccce71ba44cc679
Emile Nijssen <emile@athom.nl>
2018-07-13 11:51:11 +0200

    add capability ui properties

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ module.exports.Signal = require('./lib/Signal');
 module.exports.Media = require('./lib/Media');
 
 module.exports.getDeviceClasses = module.exports.Device.getClasses;
-module.exports.getCapabilities = module.exports.Device.getCapabilities;
+module.exports.getDeviceClass = module.exports.Device.getClass;
+
+module.exports.getCapabilities = module.exports.Capability.getCapabilities;
+module.exports.getCapability = module.exports.Capability.getCapability;
+
 module.exports.getAppLocales = module.exports.App.getLocales;
 module.exports.getAppCategories = module.exports.App.getCategories;
 module.exports.getAppPermissions = module.exports.App.getPermissions;
+
 module.exports.getMediaCodecs = module.exports.Media.getCodecs;
