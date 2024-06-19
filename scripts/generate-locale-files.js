0ed6fc31d0723c9e506fbb384c5a761ae00ffe3a
Robin Bolscher <robinbolscher@gmail.com>
2024-06-19 12:59:08 +0200

    chore: add scripts that can generate and apply locale files

---
+++ scripts/generate-locale-files.js
@@ -0,0 +1,111 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const ROOT_DIR = path.join(__dirname, '..');
+const OUTPUT_DIR = path.join(ROOT_DIR, 'generated_locales'); // Directory where you want to save the translations
+const ASSETS_PATH = path.join(ROOT_DIR, 'assets'); // Path to the assets directory
+const LOCALES = [
+  'en',
+  'nl',
+  'de',
+  'fr',
+  'it',
+  'sv',
+  'no',
+  'es',
+  'da',
+  'ru',
+  'pl',
+];
+
+// Function to recursively traverse the JSON object and extract translations
+function extractTranslations(
+  sourceFilePath,
+  obj,
+  currentPath = '',
+  translations = {},
+) {
+  Object.entries(obj).forEach(([key, value]) => {
+    const newPath = currentPath ? `${currentPath}.${key}` : key;
+    if (typeof value === 'object' && value !== null) {
+      extractTranslations(sourceFilePath, value, newPath, translations);
+    } else {
+      // Assuming every leaf node is a translation
+      const pathParts = newPath.split('.');
+      const locale = pathParts.pop(); // Assuming the last part of the path is the locale
+      if (LOCALES.includes(locale)) {
+        // Only extract translations for the specified locales
+        const translationPath = pathParts.join('.');
+        if (!translations[locale]) translations[locale] = {};
+        const relativeSourceFilePath = sourceFilePath.replace(ROOT_DIR, '.');
+        translations[locale][`${relativeSourceFilePath}@${translationPath}`] = value;
+      }
+    }
+  });
+  return translations;
+}
+
+// Function to write translations to separate files
+function writeTranslations(translations, outputDir) {
+  if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir, { recursive: true });
+  }
+  Object.entries(translations).forEach(([locale, translationObj]) => {
+    if (LOCALES.includes(locale)) {
+      // Only write translations for the specified locales
+      const filePath = path.join(outputDir, `${locale}.json`);
+      // Append to JSON file
+      let existingTranslations = {
+        __comment: 'This file is generated. Do not edit it directly.',
+      };
+      if (fs.existsSync(filePath)) {
+        existingTranslations = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+      }
+      translationObj = { ...existingTranslations, ...translationObj };
+      fs.writeFileSync(filePath, JSON.stringify(translationObj, null, 2));
+    }
+  });
+}
+
+console.log(
+  `Generating locales (${LOCALES.map(x => x.toUpperCase())}) in ${OUTPUT_DIR}...`,
+);
+
+// Remove all files in the output directory
+if (fs.existsSync(OUTPUT_DIR)) {
+  fs.rmdirSync(OUTPUT_DIR, { recursive: true });
+}
+
+// Parse permissions.json
+const sourceFilePath = path.join(ASSETS_PATH, 'app', 'permissions.json');
+const sourceContent = JSON.parse(fs.readFileSync(sourceFilePath, 'utf8'));
+const translations = extractTranslations(sourceFilePath, sourceContent);
+writeTranslations(translations, OUTPUT_DIR);
+
+// Parse capabilities
+const sourceDirCapabilities = path.join(
+  ASSETS_PATH,
+  'capability',
+  'capabilities',
+);
+const capabilitiesFiles = fs.readdirSync(sourceDirCapabilities);
+capabilitiesFiles.forEach(file => {
+  const sourceFilePath = path.join(sourceDirCapabilities, file);
+  const sourceContent = JSON.parse(fs.readFileSync(sourceFilePath, 'utf8'));
+  const translations = extractTranslations(sourceFilePath, sourceContent);
+  writeTranslations(translations, OUTPUT_DIR);
+});
+
+// Parse device classes
+const sourceDirDeviceClasses = path.join(ASSETS_PATH, 'device', 'classes');
+const deviceClassesFiles = fs.readdirSync(sourceDirDeviceClasses);
+deviceClassesFiles.forEach(file => {
+  const sourceFilePath = path.join(sourceDirDeviceClasses, file);
+  const sourceContent = JSON.parse(fs.readFileSync(sourceFilePath, 'utf8'));
+  const translations = extractTranslations(sourceFilePath, sourceContent);
+  writeTranslations(translations, OUTPUT_DIR);
+});
+
+console.log('Done generating locales.');
